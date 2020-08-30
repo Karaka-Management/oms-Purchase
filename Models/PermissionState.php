2ce725ea6c61531bf7d3dd640dac63e24544a2e9
Dennis Eichhorn <spl1nes.com@googlemail.com>
2020-08-30 20:13:11 +0200

    autofixes

--- Models/PermissionState.php
+++ Models/PermissionState.php
@@ -27,5 +27,6 @@ use phpOMS\Stdlib\Base\Enum;
 abstract class PermissionState extends Enum
 {
     public const INVOICE = 1;
+
     public const ARTICLE = 2;
 }
--- tests/Admin/AdminTest.php
+++ tests/Admin/AdminTest.php
@@ -20,6 +20,7 @@ namespace Modules\Purchase\tests\Admin;
 class AdminTest extends \PHPUnit\Framework\TestCase
 {
     protected const MODULE_NAME = 'Purchase';
+
     protected const URI_LOAD = '';
 
     use \Modules\tests\ModuleTestTrait;
